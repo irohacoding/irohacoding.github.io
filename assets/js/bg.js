e93f2207921fc799f7d8ea79abf88db0663bc4db
IrohaCoding <info@irohacoding.com>
2019-11-22 21:52:12 +0900

    Change a bit.

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -1,14 +1,14 @@
 var cTime = new Date().getHours();
-if (9 <= cTime && cTime < 15) {
+if ((6 <= cTime && cTime < 10) || (16 <= cTime && cTime < 20)) {
+  if (document.body) {
+    document.body.style.backgroundColor = "#0088aa"
+    
+} else if (11 <= cTime && cTime < 15) {
   if (document.body) {
     document.body.style.backgroundColor = "#009cc4"
   }
-} else if (15 <= cTime && cTime < 20) {
-  if (document.body) {
-    document.body.style.backgroundColor = "#0088aa"
-  }
 } else {
   if (document.body) {
-    document.body.style.backgroundColor = "#004b5e"
+    document.body.style.backgroundColor = "#006983"
   }
 }
